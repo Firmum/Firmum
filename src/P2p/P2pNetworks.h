45443fb28d8f589193d9edd12ba3ad3cecac5442
markonimus <34181845+markonimus@users.noreply.github.com>
2017-12-02 20:26:50 +0100

    dodao sam gotov projekt
    sve napravio na svom kompu

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xff, 0x03, 0xee, 0xee, 0xdd, 0x00, 0x00, 0x17, 0x00, 0x08, 0x00, 0x06, 0xa0, 0x90, 0x80, 0x03 } };
+}
